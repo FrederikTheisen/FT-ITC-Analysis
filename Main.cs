dd96c896230a48073cf27c5843e1ce81cd4fa657
Frederik Theisen <frederiktheisen@gmail.com>
2022-02-08 15:12:01 +0100

    First commit

---
+++ Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace AnalysisITC
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
