479e8d14c0ace6b0a94305902b21fd473f798c54
Frederik Theisen <frederiktheisen@gmail.com>
2022-03-12 17:02:12 +0100

    Added

---
+++ GUI/StatusBarManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AnalysisITC
+{
+    public static class StatusBarManager
+    {
+        public static event EventHandler<string> FileDidLoad;
+
+        public static void FileLoaded(string filename)
+        {
+            FileDidLoad?.Invoke(null, filename);
+        }
+    }
+}
