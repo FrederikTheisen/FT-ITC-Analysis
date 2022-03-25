5ce479ec9dc029e282dbadadd54885865f04f60e
Frederik Theisen <frederiktheisen@gmail.com>
2022-03-25 12:21:05 +0100

    Added energy math

--- Utils/NumberStructs.cs
+++ Utils/NumberStructs.cs
@@ -7,8 +7,9 @@ namespace AnalysisITC
     public struct Energy
     {
         const double CalToJouleFactor = 4.184;
+        public static readonly Energy R = new Energy(8.3145, EnergyUnit.Joule);
 
-        readonly double value;
+        private double value;
         readonly EnergyUnit unit;
 
         private FloatWithError Joule;
@@ -18,8 +19,8 @@ namespace AnalysisITC
         {
             this.unit = unit;
             value = v.Value;
-            Joule = 0;
-            Cal = 0;
+            Joule = v;
+            Cal = v;
             Value = v;
             
         }
@@ -28,9 +29,9 @@ namespace AnalysisITC
         {
             this.unit = unit;
             value = v;
-            Joule = 0;
-            Cal = 0;
-            Value = v;
+            Joule = new();
+            Cal = new();
+            Value = new(v);
 
         }
 
@@ -38,18 +39,18 @@ namespace AnalysisITC
         {
             unit = DataManager.Unit;
             value = v.Value;
-            Joule = 0;
-            Cal = 0;
-            Value = v;
+            Joule = v;
+            Cal = v;
+            Value = new(v);
         }
 
-        public Energy(double v)
+        public Energy(double v = 0)
         {
             unit = DataManager.Unit;
             value = v;
-            Joule = 0;
-            Cal = 0;
-            Value = v;
+            Joule = new();
+            Cal = new();
+            Value = new(v);
         }
 
         public FloatWithError Value
@@ -106,47 +107,34 @@ namespace AnalysisITC
             return new Energy(v, ExperimentData.Unit);
         }
 
-        public static Energy operator /(Energy e1, Energy e2)
+        public static Energy operator /(Energy e1, Energy e2) => new Energy(e1.Value / e2.Value);
+
+        public static Energy operator /(Energy e1, double val) => new Energy(e1.Value / val);
+
+        public static Energy operator *(Energy e1, Energy e2) => new Energy(e1.Value * e2.Value);
+
+        public static Energy operator *(Energy e1, double val) => new Energy(e1.Value * val);
+
+        public static Energy operator *(double val, Energy e) => new Energy(e.Value * val);
+
+        public static bool operator <(Energy v1, Energy v2) => v1.Value.Value < v2.Value.Value;
+
+        public static bool operator >(Energy v1, Energy v2) => v1.Value.Value > v2.Value.Value;
+
+        public static bool operator <(Energy v1, double v2) => v1.Value.Value < v2;
+
+        public static bool operator >(Energy v1, double v2) => v1.Value.Value > v2;
+
+        public static implicit operator double(Energy e) => e.Value.Value;
+
+        //TODO add unit to print
+        public override string ToString()
         {
-            var v = e1.Value / e2.Value;
-
-            return new Energy(v, ExperimentData.Unit);
-        }
-
-        public static Energy operator /(Energy e1, double val)
-        {
-            var v = e1.Value / val;
-
-            return new Energy(v, ExperimentData.Unit);
-        }
-
-        public static Energy operator *(Energy e1, Energy e2)
-        {
-            var v = e1.Value * e2.Value;
-
-            return new Energy(v, ExperimentData.Unit);
-        }
-
-        public static Energy operator *(Energy e1, double val)
-        {
-            var v = e1.Value * val;
-
-            return new Energy(v, ExperimentData.Unit);
-        }
-
-        public static Energy operator *(double val, Energy e)
-        {
-            var v = e.Value * val;
-
-            return new Energy(v, ExperimentData.Unit);
-        }
-
-        public static implicit operator double(Energy e)
-        {
-            return e.Value.Value;
+            return Value.ToString();
         }
     }
 
+    //TODO add attribute with unit names and stuff
     public enum EnergyUnit
     {
         Joule,
@@ -157,8 +145,17 @@ namespace AnalysisITC
     {
         public double Value { get; set; }
         public double SD { get; set; }
+        public double FractionSD
+        {
+            get
+            {
+                if (SD < double.Epsilon) return 0;
+                else if (Math.Abs(Value) < double.Epsilon) return 0;
+                else return SD / Value;
+            }
+        }
 
-        public FloatWithError(double value, double error = 0)
+        public FloatWithError(double value = 0, double error = 0)
         {
             Value = value;
             SD = error;
@@ -184,34 +181,62 @@ namespace AnalysisITC
         {
             var v = v1.Value / v2.Value;
 
-            var fv1 = v1.SD / v1.Value;
-            var fv2 = v2.SD / v2.Value;
+            if (v1.SD + v2.SD < double.Epsilon) return new FloatWithError(v, 0);
+            if (v1.SD < double.Epsilon) return new FloatWithError(v, v2.FractionSD * v);
+            if (v2.SD < double.Epsilon) return new FloatWithError(v, v1.FractionSD * v);
 
-            var sd = Math.Sqrt(fv1 * fv1 + fv2 * fv2);
+            var fv1 = v1.FractionSD;
+            var fv2 = v2.FractionSD;
+
+            var sd = v * Math.Sqrt(fv1 * fv1 + fv2 * fv2);
 
             return new FloatWithError(v, sd);
+        }
+
+        public static FloatWithError operator /(FloatWithError v1, double v2)
+        {
+            var v = v1.Value / v2;
+
+            return new FloatWithError(v, v1.FractionSD * v);
         }
 
         public static FloatWithError operator *(FloatWithError v1, FloatWithError v2)
         {
             var v = v1.Value * v2.Value;
 
-            var fv1 = v1.SD / v1.Value;
-            var fv2 = v2.SD / v2.Value;
+            if (v1.SD + v2.SD < double.Epsilon) return new FloatWithError(v, 0);
+            if (v1.SD < double.Epsilon) return new FloatWithError(v, v2.FractionSD * v);
+            if (v2.SD < double.Epsilon) return new FloatWithError(v, v1.FractionSD * v);
 
-            var sd = Math.Sqrt(fv1 * fv1 + fv2 * fv2);
+            var fv1 = v1.FractionSD;
+            var fv2 = v2.FractionSD;
+
+            var sd = v * Math.Sqrt(fv1 * fv1 + fv2 * fv2);
 
             return new FloatWithError(v, sd);
         }
 
-        public static implicit operator FloatWithError(double v)
+        public static FloatWithError operator *(FloatWithError v1, double v2)
+        {
+            var v = v1.Value * v2;
+
+            return new FloatWithError(v, v1.FractionSD * v);
+        }
+
+        public static explicit operator FloatWithError(double v)
         {
             return new FloatWithError(v);
         }
 
-        public static explicit operator double(FloatWithError v)
+        public static implicit operator double(FloatWithError v)
         {
             return v.Value;
+        }
+
+        public override string ToString()
+        {
+            if (SD < double.Epsilon) return Value.ToString();
+            else return Value.ToString() + " +- " + SD.ToString();
         }
     }
 }
