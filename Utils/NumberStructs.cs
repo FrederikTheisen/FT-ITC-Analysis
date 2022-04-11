a3f38474e33df7165345a4c8e12506b00e141c96
Frederik Theisen <frederiktheisen@gmail.com>
2022-04-12 00:10:15 +0200

    Changed energy class to alway be in joule Added log error to FWE

--- Utils/NumberStructs.cs
+++ Utils/NumberStructs.cs
@@ -7,92 +7,24 @@ namespace AnalysisITC
     public struct Energy
     {
         const double CalToJouleFactor = 4.184;
+        const double JouleToCalFactor = 1 / 4.184;
         const double MicroFactor = 0.000001;
-        public static readonly Energy R = new Energy(8.3145, EnergyUnit.Joule);
+        public static readonly Energy R = new Energy(8.3145);
 
-        private double value;
-        readonly EnergyUnit unit;
-
-        private FloatWithError Joule;
-        private FloatWithError Cal;
-
-        public Energy(FloatWithError v, EnergyUnit unit)
-        {
-            this.unit = unit;
-            value = v.Value;
-            Joule = v;
-            Cal = v;
-            Value = v;
-            
-        }
-
-        public Energy(double v, EnergyUnit unit)
-        {
-            this.unit = unit;
-            value = v;
-            Joule = new();
-            Cal = new();
-            Value = new(v);
-
-        }
+        public FloatWithError Value { get; set; }
+        public double SD => Value.SD;
 
         public Energy(FloatWithError v)
         {
-            unit = DataManager.Unit;
-            value = v.Value;
-            Joule = v;
-            Cal = v;
             Value = v;
         }
 
-        public Energy(double v = 0)
+        public Energy(double v)
         {
-            unit = DataManager.Unit;
-            value = v;
-            Joule = new();
-            Cal = new();
             Value = new(v);
         }
 
-        public FloatWithError Value
-        {
-            private set
-            {
-                switch (unit)
-                {
-                    case EnergyUnit.Joule:
-                        Joule = value;
-                        Cal = value / CalToJouleFactor;
-                        break;
-                    case EnergyUnit.Cal:
-                        Joule = value * CalToJouleFactor;
-                        Cal = value;
-                        break;
-                }
-            }
-            get
-            {
-                switch (DataManager.Unit)
-                {
-                    default:
-                    case EnergyUnit.Joule: return Joule;
-                    case EnergyUnit.Cal: return Cal;
-                }
-            }
-        }
-
-        public double SD
-        {
-            get
-            {
-                switch (DataManager.Unit)
-                {
-                    default:
-                    case EnergyUnit.Joule: return Joule.SD;
-                    case EnergyUnit.Cal: return Cal.SD;
-                }
-            }
-        }
+        public static Energy FromDistribution(IEnumerable<double> dist, double? mean = null) => new Energy(new FloatWithError(dist, mean));
 
         public static double ConvertToJoule(double value, EnergyUnit from)
         {
@@ -109,14 +41,14 @@ namespace AnalysisITC
         {
             var v = e1.Value + e2.Value;
 
-            return new Energy(v, ExperimentData.Unit);
+            return new Energy(v);
         }
 
         public static Energy operator -(Energy e1, Energy e2)
         {
             var v = e1.Value - e2.Value;
 
-            return new Energy(v, ExperimentData.Unit);
+            return new Energy(v);
         }
 
         public static Energy operator /(Energy e1, Energy e2) => new Energy(e1.Value / e2.Value);
@@ -129,13 +61,13 @@ namespace AnalysisITC
 
         public static Energy operator *(double val, Energy e) => new Energy(e.Value * val);
 
-        public static bool operator <(Energy v1, Energy v2) => v1.Value.Value < v2.Value.Value;
+        //public static bool operator <(Energy v1, Energy v2) => v1.Value.Value < v2.Value.Value;
 
-        public static bool operator >(Energy v1, Energy v2) => v1.Value.Value > v2.Value.Value;
+        //public static bool operator >(Energy v1, Energy v2) => v1.Value.Value > v2.Value.Value;
 
-        public static bool operator <(Energy v1, double v2) => v1.Value.Value < v2;
+        //public static bool operator <(Energy v1, double v2) => v1.Value.Value < v2;
 
-        public static bool operator >(Energy v1, double v2) => v1.Value.Value > v2;
+        //public static bool operator >(Energy v1, double v2) => v1.Value.Value > v2;
 
         public static implicit operator double(Energy e) => e.Value.Value;
 
@@ -144,11 +76,24 @@ namespace AnalysisITC
         {
             return Value.ToString();
         }
+
+        public string ToString(EnergyUnit unit)
+        {
+            switch (unit)
+            {
+                case EnergyUnit.Joule: return Value.ToString() + " J";
+                case EnergyUnit.MicroCal: return (1000000 * JouleToCalFactor * Value).ToString() + " µcal";
+                case EnergyUnit.Cal: return (JouleToCalFactor * Value).ToString() + " cal";
+                case EnergyUnit.KiloJoule: return (Value/1000).ToString() + " kJ";
+                default: return Value.ToString() + " J";
+            }
+        }
     }
 
     //TODO add attribute with unit names and stuff
     public enum EnergyUnit
     {
+        KiloJoule,
         Joule,
         MicroCal,
         Cal
@@ -172,6 +117,42 @@ namespace AnalysisITC
         {
             Value = value;
             SD = error;
+        }
+
+        public FloatWithError(IEnumerable<double> distribution, double? mean = null)
+        {
+            double result = 0;
+            double average = 0;
+
+            if (distribution.Any())
+            {
+                average = distribution.Average();
+                if (mean != null) average = (double)mean;
+                double sum = distribution.Sum(d => Math.Pow(d - average, 2));
+                result = Math.Sqrt((sum) / distribution.Count());
+            }
+
+            this = new FloatWithError(average, result);
+        }
+
+        public void SetError(IEnumerable<double> distribution)
+        {
+            double result = 0;
+            double average = 0;
+
+            if (distribution.Any())
+            {
+                average = distribution.Average();
+                double sum = distribution.Sum(d => Math.Pow(d - average, 2));
+                result = Math.Sqrt((sum) / distribution.Count());
+            }
+
+            SD = result;
+        }
+
+        public double[] WithConfidence()
+        {
+            return new double[] { Value + 2 * SD, Value - 2 * SD };
         }
 
         public static FloatWithError operator +(FloatWithError v1, FloatWithError v2)
@@ -229,7 +210,9 @@ namespace AnalysisITC
             return new FloatWithError(v, sd);
         }
 
-        public static FloatWithError operator *(FloatWithError v1, double v2)
+        public static FloatWithError operator *(FloatWithError v1, double v2) => v2 * v1;
+
+        public static FloatWithError operator *(double v2, FloatWithError v1)
         {
             var v = v1.Value * v2;
 
@@ -246,10 +229,22 @@ namespace AnalysisITC
             return v.Value;
         }
 
-        public override string ToString()
+        public string ToString(string format = "F1")
         {
-            if (SD < double.Epsilon) return Value.ToString();
-            else return Value.ToString() + " +- " + SD.ToString();
+            if (SD < double.Epsilon) return Value.ToString(format);
+            else return Value.ToString(format) + " ± " + SD.ToString(format);
+        }
+
+        public string AsDissociationConstant()
+        {
+            var mag = Math.Log10(Value);
+
+            if (mag > 0) return this.ToString() + " M";
+            else if (mag > -3) return (1000 * this).ToString() + " mM";
+            else if (mag > -6) return (1000000 * this).ToString() + " µM";
+            else if (mag > -9) return (1000000000 * this).ToString() + " nM";
+            else if (mag > -12) return (1000000000000 * this).ToString() + " pM";
+            else return this.ToString() + " M";
         }
     }
 }
