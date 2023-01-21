3321a4259a79ec774f484581c818700da4992838
frederiktheisen <FrederikTheisen@gmail.com>
2023-01-21 17:26:02 +0100

    Added temp span option and solver class file

---
+++ AppClasses/Analysis2/Solver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AnalysisITC.AppClasses.Analysis2
+{
+    public class Solver
+    {
+        Model Model { get; set; }
+
+        public SolverConvergence Fit(Analysis.SolverAlgorithm algorithm)
+        {
+            var starttime = DateTime.Now;
+
+            switch (algorithm)
+            {
+                case Analysis.SolverAlgorithm.NelderMead:
+                    break;
+                case Analysis.SolverAlgorithm.LevenbergMarquardt:
+                    break;
+            }
+        }
+
+        SolverConvergence SolveUsingNelderMead()
+        {
+
+        }
+    }
+}
+
--- AppSettings.cs
+++ AppSettings.cs
@@ -6,6 +6,7 @@ namespace AnalysisITC
         public static double ReferenceTemperature { get; set; } = 25.0;
 
         public static PeakFitAlgorithm PeakFitAlgorithm { get; set; } = PeakFitAlgorithm.Exponential;
+        public static double MinimumTemperatureSpanForFitting { get; internal set; } = 2;
     }
 
     public enum PeakFitAlgorithm
