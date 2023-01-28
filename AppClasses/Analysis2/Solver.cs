a4813b81b44055fe47306c691a7c47fca78c469f
frederiktheisen <FrederikTheisen@gmail.com>
2023-01-28 12:33:50 +0100

    Partly implemented boostrap error method

--- AppClasses/Analysis2/Solver.cs
+++ AppClasses/Analysis2/Solver.cs
@@ -1,28 +1,48 @@
 ﻿using System;
 using System.Linq;
 using Accord.Math.Optimization;
-using static alglib;
 //using static CoreFoundation.DispatchSource;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using AppKit;
 
 namespace AnalysisITC.AppClasses.Analysis2
 {
     public class Solver
     {
+        public static TerminationFlag TerminateAnalysisFlag { get; private set; } = new TerminationFlag();
+
+        public static event EventHandler<Tuple<int, int, float>> BootstrapIterationFinished;
+
         public Model Model { get; set; }
+        public Analysis.SolverAlgorithm SolverAlgorithm { get; set; } = Analysis.SolverAlgorithm.NelderMead;
+        public Analysis.ErrorEstimationMethod ErrorEstimationMethod { get; set; } = Analysis.ErrorEstimationMethod.BootstrapResiduals;
+        public static int BootstrapIterations { get; set; } = 100;
 
+        public static void ReportBootstrapProgress(int iteration) => NSApplication.SharedApplication.InvokeOnMainThread(() =>
+        {
+            BootstrapIterationFinished?.Invoke(null, new Tuple<int, int, float>(iteration, BootstrapIterations, iteration / (float)BootstrapIterations));
+        });
 
-        public SolverConvergence Fit(Analysis.SolverAlgorithm algorithm)
+        public SolverConvergence Solve()
         {
             var starttime = DateTime.Now;
+            TerminateAnalysisFlag = new TerminationFlag();
 
             SolverConvergence convergence;
 
-            switch (algorithm)
+            switch (SolverAlgorithm)
             {
                 case Analysis.SolverAlgorithm.NelderMead: convergence = SolveWithNelderMeadAlgorithm(); break;
                 case Analysis.SolverAlgorithm.LevenbergMarquardt:
                 default: throw new NotImplementedException("Solver algorithm not implemented");
+            }
+
+            switch (ErrorEstimationMethod)
+            {
+
             }
 
             return convergence;
@@ -51,6 +71,42 @@ namespace AnalysisITC.AppClasses.Analysis2
             return Model.Data.Solution.Convergence;
         }
 
+        void BoostrapResiduals()
+        {
+            ReportBootstrapProgress(0);
+
+            int counter = 0;
+            var start = DateTime.Now;
+            var solutions = new List<Solution>();
+
+            var bag = new ConcurrentBag<Solution>();
+
+            var res = Parallel.For(0, BootstrapIterations, (i) =>
+            {
+                if (TerminateAnalysisFlag.Down)
+                {
+                    var solver = new Solver();
+                    solver.SolverAlgorithm = this.SolverAlgorithm;
+                    solver.ErrorEstimationMethod = Analysis.ErrorEstimationMethod.None;
+                    solver.Model = Model.GenerateSyntheticModel();
+                    //solver.Model.SetBootstrapStart(Solution.Raw);
+
+                    solver.Solve();
+
+                    //bag.Add(solver.Solution);
+                }
+
+                var currcounter = Interlocked.Increment(ref counter);
+
+                ReportBootstrapProgress(currcounter);
+            });
+
+            solutions = bag.ToList();
+
+            //Solution.BootstrapSolutions = solutions.Where(sol => !sol.Convergence.Failed).ToList();
+            //Solution.ComputeErrorsFromBootstrapSolutions();
+        }
+
         private void SetStepSizes(NelderMead solver)
         {
             var stepsize = Model.Parameters.GetStepSizes();
@@ -74,7 +130,7 @@ namespace AnalysisITC.AppClasses.Analysis2
                         simplex.UpperBounds[i] = upper[i];
                     }
                     break;
-                case minlmstate state:
+                case alglib.minlmstate state:
                     alglib.minlmsetbc(state, lower, upper);
                     break;
             }
