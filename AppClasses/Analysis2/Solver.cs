e36ee5c230a73d6ee857cc473d5689fed68d480f
frederiktheisen <FrederikTheisen@gmail.com>
2023-02-10 13:17:43 +0100

    Implemented factory initialization API

--- AppClasses/Analysis2/Solver.cs
+++ AppClasses/Analysis2/Solver.cs
@@ -15,7 +15,7 @@ namespace AnalysisITC.AppClasses.Analysis2
     public class SolverInterface
     {
         public static TerminationFlag TerminateAnalysisFlag { get; protected set; } = new TerminationFlag();
-        public static int BootstrapIterations { get; set; } = 100;
+        
 
         public static event EventHandler<Tuple<int, int, float>> BootstrapIterationFinished;
         public static event EventHandler<SolverConvergence> AnalysisFinished;
@@ -23,6 +23,10 @@ namespace AnalysisITC.AppClasses.Analysis2
 
         public Analysis.SolverAlgorithm SolverAlgorithm { get; set; } = Analysis.SolverAlgorithm.NelderMead;
         public Analysis.ErrorEstimationMethod ErrorEstimationMethod { get; set; } = Analysis.ErrorEstimationMethod.None;
+        public int BootstrapIterations { get; set; } = 100;
+        public double SolverFunctionTolerance { get; set; } = double.Epsilon;
+        public double SolverBootstrapTolerance { get; set; } = 1.0E-11;
+
 
         protected DateTime starttime;
         protected DateTime endtime;
@@ -36,24 +40,54 @@ namespace AnalysisITC.AppClasses.Analysis2
             }
         }
 
+        public static SolverInterface Initialize(ModelFactory factory)
+        {
+            switch (factory)
+            {
+                case SingleModelFactory: return Initialize((factory as SingleModelFactory).Model);
+                case GlobalModelFactory: return Initialize((factory as GlobalModelFactory).Model);
+                default: throw new Exception("Solver initilization failure: unknown factory");
+            }
+        }
+
+        public static SolverInterface Initialize(Model model)
+        {
+            var solver = new Solver();
+            solver.Model = model;
+
+            return solver;
+        }
+
+        public static SolverInterface Initialize(GlobalModel model)
+        {
+            var solver = new GlobalSolver();
+            solver.Model = model;
+
+            return solver;
+        }
+
         public void ReportBootstrapProgress(int iteration) => NSApplication.SharedApplication.InvokeOnMainThread(() =>
         {
             BootstrapIterationFinished?.Invoke(null, new Tuple<int, int, float>(iteration, BootstrapIterations, iteration / (float)BootstrapIterations));
         });
 
-        public void ReportAnalysisStepFinished()
+        public void ReportAnalysisStepFinished() => NSApplication.SharedApplication.InvokeOnMainThread(() =>
         {
             AnalysisStepFinished?.Invoke(null, null);
-        }
+        });
 
-        public void ReportAnalysisFinished(SolverConvergence convergence)
+        public void ReportAnalysisFinished(SolverConvergence convergence) => NSApplication.SharedApplication.InvokeOnMainThread(() =>
         {
             AnalysisFinished?.Invoke(null, convergence);
-        }
+        });
 
-        public async void Analyze() => await Task.Run(() => { Solve(); });
+        public async void Analyze() => await Task.Run(() =>
+        {
+            var convergence = Solve();
+            ReportAnalysisFinished(convergence);
+        });
 
-        public virtual void Solve()
+        public virtual SolverConvergence Solve()
         {
             starttime = DateTime.Now;
             TerminateAnalysisFlag = new TerminationFlag();
@@ -80,7 +114,7 @@ namespace AnalysisITC.AppClasses.Analysis2
 
             Console.WriteLine("Analysis time: " + Duration.TotalSeconds + "s");
 
-            ReportAnalysisFinished(convergence);
+            return convergence;
         }
 
         protected virtual SolverConvergence SolveWithNelderMeadAlgorithm()
@@ -95,7 +129,7 @@ namespace AnalysisITC.AppClasses.Analysis2
 
         protected virtual void BoostrapResiduals()
         {
-            throw new NotImplementedException();
+            ReportBootstrapProgress(0);
         }
 
         protected void SetStepSizes(NelderMead solver, double[] stepsize)
@@ -137,7 +171,7 @@ namespace AnalysisITC.AppClasses.Analysis2
             solver.Convergence = new Accord.Math.Convergence.GeneralConvergence(Model.NumberOfParameters)
             {
                 MaximumEvaluations = 300000,
-                AbsoluteFunctionTolerance = double.Epsilon,
+                AbsoluteFunctionTolerance = SolverFunctionTolerance,
                 StartTime = DateTime.Now,
             };
 
@@ -154,7 +188,7 @@ namespace AnalysisITC.AppClasses.Analysis2
 
         protected override void BoostrapResiduals()
         {
-            ReportBootstrapProgress(0);
+            base.BoostrapResiduals();
 
             int counter = 0;
             var start = DateTime.Now;
@@ -167,6 +201,7 @@ namespace AnalysisITC.AppClasses.Analysis2
                     var solver = new Solver();
                     solver.SolverAlgorithm = this.SolverAlgorithm;
                     solver.Model = Model.GenerateSyntheticModel();
+                    solver.SolverFunctionTolerance = SolverBootstrapTolerance;
 
                     solver.Solve();
 
@@ -200,7 +235,7 @@ namespace AnalysisITC.AppClasses.Analysis2
             solver.Convergence = new Accord.Math.Convergence.GeneralConvergence(Model.NumberOfParameters)
             {
                 MaximumEvaluations = 300000,
-                AbsoluteFunctionTolerance = double.Epsilon,
+                AbsoluteFunctionTolerance = SolverFunctionTolerance,
                 StartTime = DateTime.Now,
             };
 
@@ -209,22 +244,18 @@ namespace AnalysisITC.AppClasses.Analysis2
 
             solver.Minimize(Model.Parameters.ToArray());
 
-            Model.Solution = new GlobalSolution(Model);
-            Model.Solution.Convergence = new SolverConvergence(solver);
+            Model.Solution = new GlobalSolution(Model, new SolverConvergence(solver));
 
             return Model.Solution.Convergence;
         }
 
         protected override void BoostrapResiduals()
         {
+            base.BoostrapResiduals();
+
             var bag = new ConcurrentBag<GlobalSolution>();
-
             int counter = 0;
-
-            ReportBootstrapProgress(0);
-
             var start = DateTime.Now;
-
             var opt = new ParallelOptions();
             opt.MaxDegreeOfParallelism = 10;
 
@@ -232,16 +263,16 @@ namespace AnalysisITC.AppClasses.Analysis2
             {
                 if (TerminateAnalysisFlag.Down)
                 {
-                    var globalmodel = new GlobalModel();
-
-                    foreach (var m in Model.Models) globalmodel.AddModel(m.GenerateSyntheticModel());
+                    var globalmodel = Model.GenerateSyntheticModel();
                     var solver = new GlobalSolver();
                     solver.Model = globalmodel;
                     solver.SolverAlgorithm = SolverAlgorithm;
+                    solver.SolverFunctionTolerance = SolverBootstrapTolerance;
 
-                    solver.Solve();
+                    var convergence = solver.Solve();
+                    var solution = new GlobalSolution(globalmodel, convergence);
 
-                    bag.Add(new GlobalSolution(globalmodel));
+                    bag.Add(solution);
                 }
 
                 var currcounter = Interlocked.Increment(ref counter);
