05c1ebe802fdb394f6a7595cdc81306606f6f7be
frederiktheisen <FrederikTheisen@gmail.com>
2023-02-22 14:47:44 +0100

    Implemented new solution interface

--- GUI/MacOS/PopOptionViewControllers/FittingOptionsPopoverViewController.cs
+++ GUI/MacOS/PopOptionViewControllers/FittingOptionsPopoverViewController.cs
@@ -14,7 +14,9 @@ namespace AnalysisITC
 	{
         List<ParameterValueAdjustmentView> ParameterOptionControls = new List<ParameterValueAdjustmentView>();
 
-		public FittingOptionsPopoverViewController (IntPtr handle) : base (handle)
+        partial void ErrorIterationSliderChanged(NSSlider sender) => ErrorIterationLabel.IntValue = (int)Math.Pow(10, ErrorIterationsControl.DoubleValue);
+
+        public FittingOptionsPopoverViewController (IntPtr handle) : base (handle)
 		{
 		}
 
@@ -22,28 +24,10 @@ namespace AnalysisITC
         {
             base.ViewWillAppear();
 
-            ErrorIterationsControl.DoubleValue = Math.Log10(Analysis.BootstrapIterations);
+            ErrorIterationsControl.DoubleValue = Math.Log10(FittingOptionsController.BootstrapIterations);
             ErrorIterationLabel.IntValue = (int)Math.Pow(10, ErrorIterationsControl.DoubleValue);
 
-            OStep.PlaceholderString = Analysis.Ostep.ToString();
-            HStep.PlaceholderString = Analysis.Hstep.ToString();
-            GStep.PlaceholderString = Analysis.Gstep.ToString();
-            CStep.PlaceholderString = Analysis.Cstep.ToString();
-            NStep.PlaceholderString = Analysis.Nstep.ToString();
-
-            InitOffset.StringValue = double.IsNaN(Analysis.Oinit) ? "" : Analysis.Oinit.ToString();
-            InitH.StringValue = double.IsNaN(Analysis.Hinit) ? "" : Analysis.Hinit.ToString();
-            InitN.StringValue = double.IsNaN(Analysis.Ninit) ? "" : Analysis.Ninit.ToString();
-            InitG.StringValue = double.IsNaN(Analysis.Ginit) ? "" : Analysis.Ginit.ToString();
-            InitCp.StringValue = double.IsNaN(Analysis.Cinit) ? "" : Analysis.Cinit.ToString();
-
-            InitHLock.State = Analysis.Hlock ? NSCellStateValue.On : NSCellStateValue.Off;
-            InitGLock.State = Analysis.Glock ? NSCellStateValue.On : NSCellStateValue.Off;
-            InitCpLock.State = Analysis.Clock ? NSCellStateValue.On : NSCellStateValue.Off;
-            InitOffsetLock.State = Analysis.Olock ? NSCellStateValue.On : NSCellStateValue.Off;
-            InitNLock.State = Analysis.Nlock ? NSCellStateValue.On : NSCellStateValue.Off;
-
-            ErrorMethodControl.SelectedSegment = (int)Analysis.ErrorMethod;
+            ErrorMethodControl.SelectedSegment = (int)FittingOptionsController.ErrorEstimationMethod;
 
             foreach (var par in ModelFactory.Factory.GetExposedParameters())
             {
@@ -64,33 +48,10 @@ namespace AnalysisITC
             base.ViewDidDisappear();
         }
 
-        partial void ErrorIterationSliderChanged(NSSlider sender)
-        {
-            ErrorIterationLabel.IntValue = (int)Math.Pow(10, ErrorIterationsControl.DoubleValue);
-        }
-
         partial void ApplyOptions(NSObject sender)
         {
-            Analysis.ErrorMethod = (Analysis.ErrorEstimationMethod)(int)ErrorMethodControl.SelectedSegment;
-            Analysis.BootstrapIterations = (int)Math.Pow(10, ErrorIterationsControl.DoubleValue);
-
-            if (OStep.DoubleValue > 0) Analysis.Ostep = OStep.DoubleValue;
-            if (HStep.DoubleValue > 0) Analysis.Hstep = HStep.DoubleValue;
-            if (GStep.DoubleValue > 0) Analysis.Gstep = GStep.DoubleValue;
-            if (CStep.DoubleValue > 0) Analysis.Cstep = CStep.DoubleValue;
-            if (NStep.DoubleValue > 0) Analysis.Nstep = NStep.DoubleValue;
-
-            if (!string.IsNullOrEmpty(InitOffset.StringValue.Trim())) Analysis.Oinit = InitOffset.DoubleValue; else Analysis.Oinit = double.NaN;
-            if (!string.IsNullOrEmpty(InitH.StringValue.Trim())) Analysis.Hinit = InitH.DoubleValue; else Analysis.Hinit = double.NaN;
-            if (!string.IsNullOrEmpty(InitG.StringValue.Trim())) Analysis.Ginit = InitG.DoubleValue; else Analysis.Ginit = double.NaN;
-            if (!string.IsNullOrEmpty(InitCp.StringValue.Trim())) Analysis.Cinit = InitCp.DoubleValue; else Analysis.Cinit = double.NaN;
-            if (!string.IsNullOrEmpty(InitN.StringValue.Trim())) Analysis.Ninit = InitN.DoubleValue; else Analysis.Ninit = double.NaN;
-
-            Analysis.Hlock = InitHLock.State == NSCellStateValue.On;
-            Analysis.Glock = InitGLock.State == NSCellStateValue.On;
-            Analysis.Clock = InitCpLock.State == NSCellStateValue.On;
-            Analysis.Olock = InitOffsetLock.State == NSCellStateValue.On;
-            Analysis.Nlock = InitNLock.State == NSCellStateValue.On;
+            FittingOptionsController.ErrorEstimationMethod = (ErrorEstimationMethod)(int)ErrorMethodControl.SelectedSegment;
+            FittingOptionsController.BootstrapIterations = (int)Math.Pow(10, ErrorIterationsControl.DoubleValue);
 
             foreach (var sv in ParameterOptionControls)
             {
