721a80bd497b5c6e84dd739b4d1a3d23458b5a31
frederiktheisen <FrederikTheisen@gmail.com>
2023-02-22 14:42:18 +0100

    Removed depreceated code

--- AppClasses/AnalysisClasses/SolverUtilities.cs
+++ AppClasses/AnalysisClasses/SolverUtilities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using Accord.Math.Optimization;
 using static alglib;
@@ -14,13 +15,13 @@ namespace AnalysisITC
         public TimeSpan BootstrapTime { get; private set; }
         public double Loss { get; private set; }
         public bool Failed { get; private set; } = false;
-        public Analysis.SolverAlgorithm Algorithm { get; private set; }
+        public SolverAlgorithm Algorithm { get; private set; }
 
         public void SetBootstrapTime(TimeSpan time) => BootstrapTime = time;
 
         public SolverConvergence(NelderMead solver)
         {
-            Algorithm = Analysis.SolverAlgorithm.NelderMead;
+            Algorithm = SolverAlgorithm.NelderMead;
             Iterations = solver.Convergence.Evaluations;
             Message = solver.Status.ToString();
             Time = DateTime.Now - solver.Convergence.StartTime;
@@ -31,7 +32,7 @@ namespace AnalysisITC
 
         public SolverConvergence(minlmstate state, minlmreport rep, TimeSpan time)
         {
-            Algorithm = Analysis.SolverAlgorithm.LevenbergMarquardt;
+            Algorithm = SolverAlgorithm.LevenbergMarquardt;
             Iterations = rep.iterationscount;
             Message = rep.terminationtype.ToString();
             Time = time;
@@ -40,7 +41,7 @@ namespace AnalysisITC
             Failed = rep.terminationtype != 2;
         }
 
-        public SolverConvergence(MathNet.Numerics.Optimization.NonlinearMinimizationResult result, TimeSpan time, Analysis.SolverAlgorithm algorithm)
+        public SolverConvergence(MathNet.Numerics.Optimization.NonlinearMinimizationResult result, TimeSpan time, SolverAlgorithm algorithm)
         {
             Algorithm = algorithm;
             Iterations = result.Iterations;
@@ -58,175 +59,8 @@ namespace AnalysisITC
             Message = list.First().Message;
             Time = TimeSpan.FromTicks(list.Sum(sc => sc.Time.Ticks));
             Loss = list.Sum(sc => sc.Loss);
-        }
-    }
 
-    public class SolverOptions
-    {
-        public object Model { get; set; }
-
-        public Analysis.VariableConstraint EnthalpyStyle { get; set; } = Analysis.VariableConstraint.TemperatureDependent;
-        public Analysis.VariableConstraint AffinityStyle { get; set; } = Analysis.VariableConstraint.None;
-        public Analysis.VariableConstraint NStyle { get; set; } = Analysis.VariableConstraint.None;
-
-        public int ModelCount => this.Model switch
-        {
-            GlobalModel => (Model as GlobalModel).Models.Count,
-            _ => 1,
-        };
-
-        public double MeanTemperature => this.Model switch
-        {
-            GlobalModel => (Model as GlobalModel).MeanTemperature,
-            _ => 25,
-        };
-    }
-
-    public class SolverParameters
-    {
-        SolverOptions options { get; set; }
-
-        public Analysis.VariableConstraint EnthalpyStyle => options.EnthalpyStyle;
-        public Analysis.VariableConstraint AffinityStyle => options.AffinityStyle;
-        public Analysis.VariableConstraint NStyle => options.NStyle;
-        public int ModelCount => options.ModelCount;
-
-        public List<double> Enthalpies { get; set; } = new List<double>();
-        public List<double> Gibbs { get; set; } = new List<double>();
-        public List<double> Offsets { get; set; } = new List<double>();
-        public List<double> Ns { get; set; } = new List<double>();
-
-        public double HeatCapacity { get; set; } = 0;
-
-        public SolverParameters(SolverOptions options)
-        {
-            this.options = options;
-        }
-
-        public static SolverParameters FromArray(double[] w, SolverOptions options)
-        {
-            var p = new SolverParameters(options);
-
-            int index = 0;
-
-            switch (options.EnthalpyStyle)
-            {
-                case Analysis.VariableConstraint.None:
-                    p.Enthalpies = w.Take(options.ModelCount).ToList();
-                    index += options.ModelCount;
-                    break;
-                case Analysis.VariableConstraint.TemperatureDependent:
-                    p.Enthalpies = w.Take(1).ToList();
-                    p.HeatCapacity = w.Skip(1).Take(1).First();
-                    index += 2;
-                    break;
-                case Analysis.VariableConstraint.SameForAll:
-                    p.Enthalpies = w.Take(1).ToList();
-                    index += 1;
-                    break;
-            }
-
-            switch (options.AffinityStyle)
-            {
-                case Analysis.VariableConstraint.None:
-                    p.Gibbs = w.Skip(index).Take(options.ModelCount).ToList();
-                    index += options.ModelCount;
-                    break;
-                case Analysis.VariableConstraint.TemperatureDependent:
-                case Analysis.VariableConstraint.SameForAll:
-                    p.Gibbs = w.Skip(index).Take(1).ToList();
-                    index += 1;
-                    break;
-            }
-
-            p.Offsets = w.Skip(index).Take(options.ModelCount).ToList();
-            index += options.ModelCount;
-
-            switch (options.NStyle)
-            {
-                case Analysis.VariableConstraint.SameForAll: p.Ns = w.Skip(index).Take(1).ToList(); break;
-                default: p.Ns = w.Skip(index).Take(options.ModelCount).ToList(); break;
-            }
-
-            return p;
-        }
-
-        public static SolverParameters FromIndividual(List<Solution> solutions, SolverOptions options)
-        {
-            var p = new SolverParameters(options);
-
-            p.Ns = solutions.Select(s => s.N.Value).ToList();
-            p.Enthalpies = solutions.Select(s => s.Enthalpy.Value).ToList();
-            p.Gibbs = solutions.Select(s => s.GibbsFreeEnergy.Value).ToList();
-            p.Offsets = solutions.Select(s => s.Offset.Value).ToList();
-
-            return p;
-        }
-
-        public double[] ToArray()
-        {
-            var w = new List<double>();
-
-            w.AddRange(Enthalpies);
-            if (EnthalpyStyle == Analysis.VariableConstraint.TemperatureDependent) w.Add(HeatCapacity);
-            w.AddRange(Gibbs);
-            w.AddRange(Offsets);
-            w.AddRange(Ns);
-
-            return w.ToArray();
-        }
-
-        public ParameterSet ParameterSetForModel(int modelnumber, SolverOptions options = null)
-        {
-            return new ParameterSet
-            {
-                Options = options,
-                dH = EnthalpyStyle == Analysis.VariableConstraint.None ? Enthalpies[modelnumber] : Enthalpies[0],
-                dCp = HeatCapacity,
-                dG = AffinityStyle == Analysis.VariableConstraint.None ? Gibbs[modelnumber] : Gibbs[0],
-                Offset = Offsets[modelnumber],
-                N = NStyle == Analysis.VariableConstraint.None ? Ns[modelnumber] : Ns[0]
-            };
-        }
-
-        public struct ParameterSet
-        {
-            public SolverOptions Options { get; set; }
-
-            public double dH { get; set; }
-            public double dCp { get; set; }
-            public double dG { get; set; }
-            public double Offset { get; set; }
-            public double N { get; set; }
-
-            public double GetEnthalpy(Model model, SolverOptions options)
-            {
-                switch (options.EnthalpyStyle)
-                {
-                    case Analysis.VariableConstraint.TemperatureDependent:
-                        var dt = model.Data.MeasuredTemperature - options.MeanTemperature;
-                        return dH + dCp * dt;
-                    default: return dH;
-                }
-            }
-
-            public double GetK(Model model, SolverOptions options)
-            {
-                var T = model.Data.MeasuredTemperature + 273.15;
-
-                switch (options.AffinityStyle)
-                {
-                    case Analysis.VariableConstraint.SameForAll: return Math.Exp(-1 * dG / (Energy.R.Value * 298.15));
-                    case Analysis.VariableConstraint.TemperatureDependent: return Math.Exp(-1 * dG / (Energy.R.Value * T));
-                    default:
-                    case Analysis.VariableConstraint.None: return Math.Exp(-1 * dG / (Energy.R.Value * T));
-                }
-            }
-
-            public double[] ToArray(Model model)
-            {
-                return new double[] { N, GetK(model, Options), dH, Offset};
-            }
+            Failed = list.Any(con => con.Failed);
         }
     }
 
@@ -251,5 +85,56 @@ namespace AnalysisITC
         {
             FlagIsRaised = false;
         }
+    }
+
+    public class SolverUpdate
+    {
+        public string Message { get; set; } = "";
+        public float Progress { get; set; } = -1;
+        public int Step { get; set; } = 0;
+        public int TotalSteps { get; set; } = 1;
+        public int Time { get; set; } = 0;
+
+        public string ProgressString => Step.ToString() + "/" + TotalSteps.ToString();
+
+        public void SendToStatusBar()
+        {
+            StatusBarManager.Progress = Progress;
+            if (Message != "") StatusBarManager.SetStatus(Message, Time);
+            if (TotalSteps > 0) StatusBarManager.SetSecondaryStatus(ProgressString, Time);
+        }
+    }
+
+    public enum AnalysisModel
+    {
+        OneSetOfSites,
+        TwoSetsOfSites,
+        SequentialBindingSites,
+        Dissociation
+    }
+
+    [Description]
+    public enum VariableConstraint
+    {
+        [Description("None")]
+        None,
+        [Description("Temperature dependent")]
+        TemperatureDependent,
+        [Description("Same for all")]
+        SameForAll
+    }
+
+    public enum ErrorEstimationMethod
+    {
+        None,
+        BootstrapResiduals
+    }
+
+    public enum SolverAlgorithm
+    {
+        [Description("Nelder-Mead [SIMPLEX]")]
+        NelderMead,
+        [Description("Levenberg-Marquardt")]
+        LevenbergMarquardt
     }
 }
