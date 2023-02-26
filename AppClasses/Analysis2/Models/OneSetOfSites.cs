adc2cee67a9a78918d30357285eb3e918c23791b
frederiktheisen <FrederikTheisen@gmail.com>
2023-02-26 12:57:50 +0100

    Moved models to individual files

---
+++ AppClasses/Analysis2/Models/OneSetOfSites.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnalysisITC.AppClasses.Analysis2
+{
+    public class OneSetOfSites : Model
+	{
+        public OneSetOfSites(ExperimentData data) : base(data)
+		{
+		}
+
+        public override void InitializeParameters(ExperimentData data)
+        {
+			base.InitializeParameters(data);
+
+			Parameters.AddParameter(ParameterTypes.Nvalue1, this.GuessN());
+            Parameters.AddParameter(ParameterTypes.Enthalpy1, this.GuessEnthalpy());
+            Parameters.AddParameter(ParameterTypes.Affinity1, this.GuessAffinity());
+            Parameters.AddParameter(ParameterTypes.Offset, this.GuessOffset());
+        }
+
+        public override double Evaluate(int injectionindex, bool withoffset = true)
+		{
+			if (withoffset) return GetDeltaHeat(injectionindex, Parameters.Table[ParameterTypes.Nvalue1].Value, Parameters.Table[ParameterTypes.Enthalpy1].Value, Parameters.Table[ParameterTypes.Affinity1].Value) + Parameters.Table[ParameterTypes.Offset].Value * Data.Injections[injectionindex].InjectionMass;
+			else return GetDeltaHeat(injectionindex, Parameters.Table[ParameterTypes.Nvalue1].Value, Parameters.Table[ParameterTypes.Enthalpy1].Value, Parameters.Table[ParameterTypes.Affinity1].Value);
+        }
+
+		public double GetDeltaHeat(int i, double n, double H, double K)
+		{
+			var inj = Data.Injections[i];
+			var Qi = GetHeatContent(inj, n, H, K);
+			var Q_i = i == 0 ? 0.0 : GetHeatContent(Data.Injections[i - 1], n, H, K);
+
+			var dQi = Qi + (inj.Volume / Data.CellVolume) * ((Qi + Q_i) / 2.0) - Q_i;
+
+			return dQi;
+		}
+
+		public double GetHeatContent(InjectionData inj, double n, double H, double K)
+		{
+			var ncell = n * inj.ActualCellConcentration;
+			var first = (ncell * H * Data.CellVolume) / 2.0;
+			var XnM = inj.ActualTitrantConcentration / ncell;
+			var nKM = 1.0 / (K * ncell);
+			var square = (1.0 + XnM + nKM);
+			var root = (square * square) - 4.0 * XnM;
+
+			return first * (1 + XnM + nKM - Math.Sqrt(root));
+		}
+
+        public override Model GenerateSyntheticModel()
+        {
+			Model mdl = new OneSetOfSites(Data.GetSynthClone());
+
+			foreach (var par in Parameters.Table)
+			{
+				mdl.Parameters.AddParameter(par.Key, par.Value.Value, par.Value.IsLocked, par.Value.Limits, par.Value.StepSize);
+			}
+
+			return mdl;
+        }
+
+        public class ModelSolution : SolutionInterface
+        {
+            public Energy Enthalpy => Parameters[ParameterTypes.Enthalpy1].Energy;
+            public FloatWithError K => Parameters[ParameterTypes.Affinity1];
+            public FloatWithError N => Parameters[ParameterTypes.Nvalue1];
+            public Energy Offset => Parameters[ParameterTypes.Offset].Energy;
+
+            public FloatWithError Kd => new FloatWithError(1) / K;
+            public Energy GibbsFreeEnergy => new(-1.0 * Energy.R.FloatWithError * TempKelvin * FWEMath.Log(K));
+            public Energy TdS => GibbsFreeEnergy - Enthalpy;
+            public Energy Entropy => TdS / TempKelvin;
+
+            public ModelSolution(Model model, double[] parameters)
+            {
+                Model = model;
+                BootstrapSolutions = new List<SolutionInterface>();
+            }
+
+            public override void ComputeErrorsFromBootstrapSolutions()
+            {
+                var enthalpies = BootstrapSolutions.Select(s => (s as ModelSolution).Enthalpy.FloatWithError.Value);
+                var k = BootstrapSolutions.Select(s => (s as ModelSolution).K.Value);
+                var n = BootstrapSolutions.Select(s => (s as ModelSolution).N.Value);
+                var offsets = BootstrapSolutions.Select(s => (s as ModelSolution).Offset.Value);
+
+                Parameters[ParameterTypes.Enthalpy1] = new FloatWithError(enthalpies, Enthalpy);
+                Parameters[ParameterTypes.Affinity1] = new FloatWithError(k, K);
+                Parameters[ParameterTypes.Nvalue1] = new FloatWithError(n, N);
+                Parameters[ParameterTypes.Offset] = new FloatWithError(offsets, Offset);
+
+                base.ComputeErrorsFromBootstrapSolutions();
+            }
+
+            public override List<Tuple<string, string>> UISolutionParameters(SolutionInfo info)
+            {
+                var output = base.UISolutionParameters(info);
+
+                if ((int)info > 0) output.Add(new("N", N.ToString("F2")));
+
+                output.Add(new("Kd", Kd.AsDissociationConstant()));
+                output.Add(new("∆H", Enthalpy.ToString(EnergyUnit.KiloJoule, permole: true)));
+
+                if ((int)info > 0)
+                {
+                    output.Add(new("-T∆S", TdS.ToString(EnergyUnit.KiloJoule, permole: true)));
+                    output.Add(new("∆G", GibbsFreeEnergy.ToString(EnergyUnit.KiloJoule, permole: true)));
+                }
+
+                if ((int)info > 1)
+                {
+                    output.Add(new("Offset", Offset.ToString(EnergyUnit.KiloJoule, permole: true)));
+                }
+
+                return output;
+            }
+
+            public override List<Tuple<ParameterTypes, Func<SolutionInterface, FloatWithError>>> DependenciesToReport => new List<Tuple<ParameterTypes, Func<SolutionInterface, FloatWithError>>>
+                {
+                    new (ParameterTypes.Enthalpy1, new(sol => (sol as ModelSolution).Enthalpy.FloatWithError)), 
+                    new (ParameterTypes.EntropyContribution1, new(sol => (sol as ModelSolution).TdS.FloatWithError)),
+                    new (ParameterTypes.Gibbs1, new(sol => (sol as ModelSolution).GibbsFreeEnergy.FloatWithError)),
+                };
+
+            public override Dictionary<ParameterTypes, FloatWithError> ReportParameters => new Dictionary<ParameterTypes, FloatWithError>
+                {
+                    { ParameterTypes.Nvalue1, N },
+                    { ParameterTypes.Affinity1, Kd },
+                    { ParameterTypes.Enthalpy1, Enthalpy.FloatWithError },
+                    { ParameterTypes.EntropyContribution1, TdS.FloatWithError} ,
+                    { ParameterTypes.Gibbs1, GibbsFreeEnergy.FloatWithError },
+                };
+        }
+    }
+}
+
---
+++ AppClasses/Analysis2/Models/TwoSetsOfSites.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnalysisITC.AppClasses.Analysis2
+{
+    public class TwoSetsOfSites : Model
+	{
+		public TwoSetsOfSites(ExperimentData data) : base(data)
+		{
+			throw new NotImplementedException("TwoSetsOfSites not implemented yet");
+		}
+
+		public override void InitializeParameters(ExperimentData data)
+		{
+            base.InitializeParameters(data);
+
+            Parameters.AddParameter(ParameterTypes.Nvalue1, this.GuessN(), limits: new double[] { 0.1, 10 });
+            Parameters.AddParameter(ParameterTypes.Enthalpy1, this.GuessEnthalpy() / 2, limits: new double[] { -500000, 500000 });
+            Parameters.AddParameter(ParameterTypes.Affinity1, this.GuessAffinity(), limits: new double[] { 10E-12, 0.1 });
+            Parameters.AddParameter(ParameterTypes.Nvalue2, this.GuessN(), limits: new double[] { 0.1, 10 });
+            Parameters.AddParameter(ParameterTypes.Enthalpy2, this.GuessEnthalpy() / 2, limits: new double[] { -500000, 500000 });
+            Parameters.AddParameter(ParameterTypes.Affinity2, this.GuessAffinity(), limits: new double[] { 10E-12, 0.1 });
+            Parameters.AddParameter(ParameterTypes.Offset, this.GuessOffset(), limits: new double[] { -500000, 500000 });
+        }
+
+        public override Model GenerateSyntheticModel()
+        {
+            return new TwoSetsOfSites(Data.GetSynthClone());
+        }
+
+		public class ModelSolution : SolutionInterface
+		{
+            public new List<ModelSolution> BootstrapSolutions { get; set; }
+
+            public Energy Enthalpy1 => new(Parameters[ParameterTypes.Enthalpy1]);
+            public Energy Enthalpy2 => new(Parameters[ParameterTypes.Enthalpy2]);
+            public FloatWithError K1 => Parameters[ParameterTypes.Affinity1];
+            public FloatWithError K2 => Parameters[ParameterTypes.Affinity2];
+            public FloatWithError N1 => Parameters[ParameterTypes.Nvalue1];
+            public FloatWithError N2 => Parameters[ParameterTypes.Nvalue2];
+            public Energy Offset => new(Parameters[ParameterTypes.Offset]);
+
+            public FloatWithError Kd1 => new FloatWithError(1) / K1;
+            public Energy GibbsFreeEnergy1 => new(-1.0 * Energy.R.FloatWithError * TempKelvin * FWEMath.Log(K1));
+            public Energy TdS1 => GibbsFreeEnergy1 - Enthalpy1;
+            public Energy Entropy1 => TdS1 / TempKelvin;
+
+            public FloatWithError Kd2 => new FloatWithError(1) / K2;
+            public Energy GibbsFreeEnergy2 => new(-1.0 * Energy.R.FloatWithError * TempKelvin * FWEMath.Log(K2));
+            public Energy TdS2 => GibbsFreeEnergy2 - Enthalpy2;
+            public Energy Entropy2 => TdS2 / TempKelvin;
+
+            public ModelSolution(Model model, double[] parameters)
+            {
+                Model = model;
+            }
+
+            public override void ComputeErrorsFromBootstrapSolutions()
+            {
+                var enthalpies1 = BootstrapSolutions.Select(s => s.Enthalpy1.FloatWithError.Value);
+                var enthalpies2 = BootstrapSolutions.Select(s => s.Enthalpy2.FloatWithError.Value);
+                var k1 = BootstrapSolutions.Select(s => s.K1.Value);
+                var k2 = BootstrapSolutions.Select(s => s.K2.Value);
+                var n1 = BootstrapSolutions.Select(s => s.N1.Value);
+                var n2 = BootstrapSolutions.Select(s => s.N2.Value);
+                var offsets = BootstrapSolutions.Select(s => (double)s.Offset);
+
+                Parameters[ParameterTypes.Enthalpy1] = new FloatWithError(enthalpies1, Enthalpy1);
+                Parameters[ParameterTypes.Affinity1] = new FloatWithError(k1, K1);
+                Parameters[ParameterTypes.Nvalue1] = new FloatWithError(n1, N1);
+                Parameters[ParameterTypes.Enthalpy1] = new FloatWithError(enthalpies2, Enthalpy2);
+                Parameters[ParameterTypes.Affinity1] = new FloatWithError(k2, K2);
+                Parameters[ParameterTypes.Nvalue1] = new FloatWithError(n2, N2);
+                Parameters[ParameterTypes.Offset] = new FloatWithError(offsets, Offset);
+
+                base.ComputeErrorsFromBootstrapSolutions();
+            }
+
+            public override List<Tuple<string, string>> UISolutionParameters(SolutionInfo info)
+            {
+                var output = base.UISolutionParameters(info);
+
+                output.Add(new("Kd1", Kd1.ToString()));
+                output.Add(new("∆H1", Enthalpy1.ToString()));
+
+                return output;
+            }
+        }
+    }
+}
+
