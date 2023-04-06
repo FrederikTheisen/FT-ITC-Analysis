4472d9ba2d8989cf5738868029b522d2df600b70
frederiktheisen <FrederikTheisen@gmail.com>
2023-04-06 11:11:31 +0200

    Renamed ParameterTypes to ParameterType and added IsEnergyParameter method

--- AppClasses/AnalysisClasses/ParameterSet.cs
+++ AppClasses/AnalysisClasses/ParameterSet.cs
@@ -9,14 +9,14 @@ namespace AnalysisITC.AppClasses.Analysis2
 {
     public class Parameter
     {
-        public ParameterTypes Key { get; private set; }
+        public ParameterType Key { get; private set; }
         public double Value { get; private set; }
         public bool IsLocked { get; private set; }
         public double[] Limits { get; private set; }
         public double StepSize { get; private set; }
         public bool Changed { get; set; } = true;
 
-        public Parameter(ParameterTypes key, double value, bool islocked = false)
+        public Parameter(ParameterType key, double value, bool islocked = false)
         {
             Key = key;
             Value = value;
@@ -84,7 +84,7 @@ namespace AnalysisITC.AppClasses.Analysis2
             return Key.ToString() + ": " + Value.ToString("G3");
         }
 
-        public static bool Equal(ParameterTypes t1, ParameterTypes t2)
+        public static bool Equal(ParameterType t1, ParameterType t2)
         {
             if (t1 == t2) return true;
             else if (t1.GetProperties().ParentType == t2) return true;
@@ -96,7 +96,7 @@ namespace AnalysisITC.AppClasses.Analysis2
     public class ModelParameters
     {
         public AnalysisModel ModelType { get; set; } = AnalysisModel.OneSetOfSites;
-        public Dictionary<ParameterTypes, Parameter> Table { get; set; } = new Dictionary<ParameterTypes, Parameter>();
+        public Dictionary<ParameterType, Parameter> Table { get; set; } = new Dictionary<ParameterType, Parameter>();
         public double ExperimentTemperature { get; private set; }
 
         public int FittingParameterCount => Table.Count(p => !p.Value.IsLocked);
@@ -106,7 +106,7 @@ namespace AnalysisITC.AppClasses.Analysis2
             ExperimentTemperature = data.MeasuredTemperatureKelvin;
         }
 
-        public void AddParameter(ParameterTypes key, double value, bool islocked = false)
+        public void AddParameter(ParameterType key, double value, bool islocked = false)
         {
             Table.Add(key, new Parameter(key, value, islocked));
         }
@@ -134,7 +134,7 @@ namespace AnalysisITC.AppClasses.Analysis2
         {
             var w = new List<double>();
 
-            foreach (KeyValuePair<ParameterTypes, Parameter> parameter in Table)
+            foreach (KeyValuePair<ParameterType, Parameter> parameter in Table)
             {
                 if (!parameter.Value.IsLocked) w.Add(parameter.Value.Value);
             }
@@ -156,10 +156,10 @@ namespace AnalysisITC.AppClasses.Analysis2
     public class GlobalModelParameters
     {
         //Settings
-        public Dictionary<ParameterTypes, VariableConstraint> Constraints { get; private set; } = new Dictionary<ParameterTypes, VariableConstraint>();
+        public Dictionary<ParameterType, VariableConstraint> Constraints { get; private set; } = new Dictionary<ParameterType, VariableConstraint>();
 
         //Properties
-        public Dictionary<ParameterTypes, Parameter> GlobalTable { get; private set; } = new Dictionary<ParameterTypes, Parameter>();
+        public Dictionary<ParameterType, Parameter> GlobalTable { get; private set; } = new Dictionary<ParameterType, Parameter>();
         public List<ModelParameters> IndividualModelParameterList { get; private set; } = new List<ModelParameters>();
 
         //Property derived values
@@ -171,7 +171,7 @@ namespace AnalysisITC.AppClasses.Analysis2
 
         public void ClearGlobalTable() => GlobalTable.Clear();
 
-        public void AddorUpdateGlobalParameter(ParameterTypes key, double value, bool islocked = false, double[] limits = null)
+        public void AddorUpdateGlobalParameter(ParameterType key, double value, bool islocked = false, double[] limits = null)
         {
             if (GlobalTable.Keys.Contains(key)) GlobalTable[key] = new Parameter(key, value, islocked);           
             else GlobalTable.Add(key, new Parameter(key, value, islocked));
@@ -182,13 +182,13 @@ namespace AnalysisITC.AppClasses.Analysis2
             IndividualModelParameterList.Add(parameters);
         }
 
-        public VariableConstraint GetConstraintForParameter(ParameterTypes key)
+        public VariableConstraint GetConstraintForParameter(ParameterType key)
         {
             if (Constraints.ContainsKey(key)) return Constraints[key];
             else return VariableConstraint.None;
         }
 
-        public void SetConstraintForParameter(ParameterTypes key, VariableConstraint constraint)
+        public void SetConstraintForParameter(ParameterType key, VariableConstraint constraint)
         {
             Constraints[key] = constraint;
         }
@@ -230,13 +230,13 @@ namespace AnalysisITC.AppClasses.Analysis2
                 {
                     switch (par.Key)
                     {
-                        case ParameterTypes.Nvalue1:
-                        case ParameterTypes.Nvalue2:
+                        case ParameterType.Nvalue1:
+                        case ParameterType.Nvalue2:
                             if (GetConstraintForParameter(par.Key) == VariableConstraint.SameForAll)
                                 par.Value.SetGlobal(GlobalTable[par.Key].Value);
                             break;
-                        case ParameterTypes.Enthalpy1:
-                        case ParameterTypes.Enthalpy2:
+                        case ParameterType.Enthalpy1:
+                        case ParameterType.Enthalpy2:
                             if (GetConstraintForParameter(par.Key) == VariableConstraint.TemperatureDependent)
                             {
                                 var refT = ReferenceTemperature;
@@ -244,8 +244,8 @@ namespace AnalysisITC.AppClasses.Analysis2
                                 var dT = mdlT - refT;
                                 var dH = par.Key switch
                                 {
-                                    ParameterTypes.Enthalpy2 => GlobalTable[ParameterTypes.Enthalpy2].Value + dT * GlobalTable[ParameterTypes.HeatCapacity2].Value,
-                                    _ => GlobalTable[ParameterTypes.Enthalpy1].Value + dT * GlobalTable[ParameterTypes.HeatCapacity1].Value,
+                                    ParameterType.Enthalpy2 => GlobalTable[ParameterType.Enthalpy2].Value + dT * GlobalTable[ParameterType.HeatCapacity2].Value,
+                                    _ => GlobalTable[ParameterType.Enthalpy1].Value + dT * GlobalTable[ParameterType.HeatCapacity1].Value,
                                 };
                                 par.Value.SetGlobal(dH);
                             }
@@ -254,10 +254,10 @@ namespace AnalysisITC.AppClasses.Analysis2
                             else if (GlobalTable.ContainsKey(par.Key) && !double.IsNaN(GlobalTable[par.Key].Value))
                                 par.Value.Update(GlobalTable[par.Key].Value, GlobalTable[par.Key].IsLocked);
                             break;
-                        case ParameterTypes.Affinity1:
-                        case ParameterTypes.Affinity2:
+                        case ParameterType.Affinity1:
+                        case ParameterType.Affinity2:
                             if (GetConstraintForParameter(par.Key) == VariableConstraint.TemperatureDependent)
-                                par.Value.SetGlobal(Math.Exp(-GlobalTable[ParameterTypes.Gibbs1].Value / (Energy.R * paramset.ExperimentTemperature)));
+                                par.Value.SetGlobal(Math.Exp(-GlobalTable[ParameterType.Gibbs1].Value / (Energy.R * paramset.ExperimentTemperature)));
                             break;
                     }
                 }
@@ -272,7 +272,7 @@ namespace AnalysisITC.AppClasses.Analysis2
         {
             var w = new List<double>();
 
-            foreach (KeyValuePair<ParameterTypes, Parameter> parameter in GlobalTable)
+            foreach (KeyValuePair<ParameterType, Parameter> parameter in GlobalTable)
             {
                 if (!parameter.Value.IsLocked) w.Add(parameter.Value.Value);
             }
@@ -289,7 +289,7 @@ namespace AnalysisITC.AppClasses.Analysis2
         {
             var w = new List<Parameter>();
 
-            foreach (KeyValuePair<ParameterTypes, Parameter> parameter in GlobalTable)
+            foreach (KeyValuePair<ParameterType, Parameter> parameter in GlobalTable)
             {
                 if (!parameter.Value.IsLocked) w.Add(parameter.Value);
             }
@@ -334,13 +334,13 @@ namespace AnalysisITC.AppClasses.Analysis2
         }
     }
 
-    public class ParameterTypesAttribute : DescriptionAttribute
+    public class ParameterTypeAttribute : DescriptionAttribute
     {
         public double DefaultStepSize { get; private set; }
         public double[] DefaultLimits { get; private set; }
-        public ParameterTypes ParentType { get; private set; }
+        public ParameterType ParentType { get; private set; }
 
-        public ParameterTypesAttribute(string name, ParameterTypes parent) : base(name)
+        public ParameterTypeAttribute(string name, ParameterType parent) : base(name)
         {
             var att = parent.GetProperties();
 
@@ -350,7 +350,7 @@ namespace AnalysisITC.AppClasses.Analysis2
             ParentType = parent;
         }
 
-        public ParameterTypesAttribute(string name, double stepsize, double[] limits, ParameterTypes parent) : base(name)
+        public ParameterTypeAttribute(string name, double stepsize, double[] limits, ParameterType parent) : base(name)
         {
             DefaultStepSize = stepsize;
             DefaultLimits = limits;
@@ -358,75 +358,88 @@ namespace AnalysisITC.AppClasses.Analysis2
             ParentType = parent;
         }
 
-        public static string TableHeaderTitle(ParameterTypes key, bool containstwo)
+        public static string TableHeaderTitle(ParameterType key, bool containstwo)
         {
             switch (key)
             {
-                case ParameterTypes.Nvalue1: return "N" + (containstwo ? "1" : "");
-                case ParameterTypes.Nvalue2: return "N2";
-                case ParameterTypes.Enthalpy1: return "∆H" + (containstwo ? "1" : "");
-                case ParameterTypes.Enthalpy2: return "∆H2";
-                case ParameterTypes.Affinity1: return "Kd" + (containstwo ? "1" : "");
-                case ParameterTypes.Affinity2: return "Kd2";
-                case ParameterTypes.EntropyContribution1: return "-T∆S" + (containstwo ? "1" : "");
-                case ParameterTypes.EntropyContribution2: return "-T∆S2";
-                case ParameterTypes.Gibbs1: return "∆G" + (containstwo ? "1" : "");
-                case ParameterTypes.Gibbs2: return "∆G2";
+                case ParameterType.Nvalue1: return "N" + (containstwo ? "1" : "");
+                case ParameterType.Nvalue2: return "N2";
+                case ParameterType.Enthalpy1: return "∆H" + (containstwo ? "1" : "");
+                case ParameterType.Enthalpy2: return "∆H2";
+                case ParameterType.Affinity1: return "Kd" + (containstwo ? "1" : "");
+                case ParameterType.Affinity2: return "Kd2";
+                case ParameterType.EntropyContribution1: return "-T∆S" + (containstwo ? "1" : "");
+                case ParameterType.EntropyContribution2: return "-T∆S2";
+                case ParameterType.Gibbs1: return "∆G" + (containstwo ? "1" : "");
+                case ParameterType.Gibbs2: return "∆G2";
                 default: throw new NotImplementedException("TableHeaderNotImplementedException: " + key.ToString());
             }
         }
 
-        public static string TableHeader(ParameterTypes key, bool containstwo, EnergyUnit energyunit, string kdunit)
+        public static string TableHeader(ParameterType key, bool containstwo, EnergyUnit energyunit, string kdunit)
         {
             string s = TableHeaderTitle(key, containstwo);
 
             switch (key.GetProperties().ParentType)
             {
                 default:
-                case ParameterTypes.Nvalue1: return s;
-                case ParameterTypes.Affinity1:  return s + " (" + kdunit + ")";
-                case ParameterTypes.Enthalpy1: return s + " (" + energyunit.GetUnit() + "/mol)";
-                case ParameterTypes.Gibbs1: return s + " (" + energyunit.GetUnit() + "/mol)";
-                case ParameterTypes.EntropyContribution1: return s + " (" + energyunit.GetUnit() + "/mol)";
+                case ParameterType.Nvalue1: return s;
+                case ParameterType.Affinity1:  return s + " (" + kdunit + ")";
+                case ParameterType.Enthalpy1: return s + " (" + energyunit.GetUnit() + "/mol)";
+                case ParameterType.Gibbs1: return s + " (" + energyunit.GetUnit() + "/mol)";
+                case ParameterType.EntropyContribution1: return s + " (" + energyunit.GetUnit() + "/mol)";
+            }
+        }
+
+        public static bool IsEnergyUnitParameter(ParameterType parameter)
+        {
+            switch (parameter.GetProperties().ParentType)
+            {
+                case ParameterType.Enthalpy1:
+                case ParameterType.Offset:
+                case ParameterType.HeatCapacity1:
+                case ParameterType.Gibbs1:
+                case ParameterType.EntropyContribution1: return true;
+                default: return false;
             }
         }
     }
 
-    public enum ParameterTypes
+    public enum ParameterType
     {
-        [ParameterTypesAttribute("N-value", 0.05, new double[] { 0.1, 10 }, ParameterTypes.Nvalue1)]
+        [ParameterTypeAttribute("N-value", 0.05, new double[] { 0.1, 10 }, ParameterType.Nvalue1)]
         Nvalue1,
-        [ParameterTypesAttribute("N-value 2", ParameterTypes.Nvalue1)]
+        [ParameterTypeAttribute("N-value 2", ParameterType.Nvalue1)]
         Nvalue2,
-        [ParameterTypesAttribute("Enthalpy", 1000, new double[] { -300000, 300000 }, ParameterTypes.Enthalpy1)]
+        [ParameterTypeAttribute("Enthalpy", 1000, new double[] { -300000, 300000 }, ParameterType.Enthalpy1)]
         Enthalpy1,
-        [ParameterTypesAttribute("Enthalpy 2", ParameterTypes.Enthalpy1)]
+        [ParameterTypeAttribute("Enthalpy 2", ParameterType.Enthalpy1)]
         Enthalpy2,
-        [ParameterTypesAttribute("Affinity", 100000, new double[] { 10, 100000000000 }, ParameterTypes.Affinity1)]
+        [ParameterTypeAttribute("Affinity", 100000, new double[] { 10, 100000000000 }, ParameterType.Affinity1)]
         Affinity1,
-        [ParameterTypesAttribute("Affinity 2", ParameterTypes.Affinity1)]
+        [ParameterTypeAttribute("Affinity 2", ParameterType.Affinity1)]
         Affinity2,
-        [ParameterTypesAttribute("Offset", 500, new double[] { -30000, 30000 }, ParameterTypes.Offset)]
+        [ParameterTypeAttribute("Offset", 500, new double[] { -30000, 30000 }, ParameterType.Offset)]
         Offset,
-        [ParameterTypesAttribute("Heat capacity", 500, new double[] { -20000, 20000 }, ParameterTypes.HeatCapacity1)]
+        [ParameterTypeAttribute("Heat capacity", 500, new double[] { -20000, 20000 }, ParameterType.HeatCapacity1)]
         HeatCapacity1,
-        [ParameterTypesAttribute("Heat capacity 2", ParameterTypes.HeatCapacity1)]
+        [ParameterTypeAttribute("Heat capacity 2", ParameterType.HeatCapacity1)]
         HeatCapacity2,
-        [ParameterTypesAttribute("Gibbs free energy", 500, new double[] { -100000, -10000 }, ParameterTypes.Gibbs1)]
+        [ParameterTypeAttribute("Gibbs free energy", 500, new double[] { -100000, -10000 }, ParameterType.Gibbs1)]
         Gibbs1,
-        [ParameterTypesAttribute("Gibbs free energy 2", ParameterTypes.Gibbs1)]
+        [ParameterTypeAttribute("Gibbs free energy 2", ParameterType.Gibbs1)]
         Gibbs2,
-        [ParameterTypesAttribute("Entropy", 5, null, ParameterTypes.Entropy1)]
+        [ParameterTypeAttribute("Entropy", 5, null, ParameterType.Entropy1)]
         Entropy1,
-        [ParameterTypesAttribute("Entropy 2", ParameterTypes.Entropy1)]
+        [ParameterTypeAttribute("Entropy 2", ParameterType.Entropy1)]
         Entropy2,
-        [ParameterTypesAttribute("Entropy contribution", 1000, null, EntropyContribution1)]
+        [ParameterTypeAttribute("Entropy contribution", 1000, null, EntropyContribution1)]
         EntropyContribution1,
-        [ParameterTypesAttribute("Entropy contribution 2", EntropyContribution1)]
+        [ParameterTypeAttribute("Entropy contribution 2", EntropyContribution1)]
         EntropyContribution2,
-        [ParameterTypesAttribute("Isomerization rate constant", 0.00001, new double[] { 0.00001, 1 }, IsomerizationRate)]
+        [ParameterTypeAttribute("Isomerization rate constant", 0.00001, new double[] { 0.00001, 1 }, IsomerizationRate)]
         IsomerizationRate,
-        [ParameterTypesAttribute("Isomerization equilibrium constant", 0.001, new double[] { 0.001, 1000 }, IsomerizationEquilibriumConstant)]
+        [ParameterTypeAttribute("Isomerization equilibrium constant", 0.001, new double[] { 0.001, 1000 }, IsomerizationEquilibriumConstant)]
         IsomerizationEquilibriumConstant,
     }
 }
