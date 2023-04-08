b5c8c5563fe795c7b1495154da6fed3bc93dc52d
frederiktheisen <FrederikTheisen@gmail.com>
2023-04-08 20:54:12 +0200

    Added Buffer attribute

---
+++ Utils/Buffers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnalysisITC.Utils
+{
+	public enum Buffers
+	{
+		Hepes,
+		Phosphate,
+		Tris,
+		BisTris,
+		Acetate
+	}
+
+	public class BufferAttribute : Attribute
+	{
+		public static List<Buffers> GetBuffers()
+		{
+			return new List<Buffers>
+			{
+				Buffers.Hepes,
+				Buffers.Phosphate,
+				Buffers.Tris,
+				Buffers.BisTris,
+				Buffers.Acetate
+			};
+		}
+	}
+}
+
