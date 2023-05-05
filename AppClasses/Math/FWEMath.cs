74685059089febf255702401879f2f1856bad504
frederiktheisen <FrederikTheisen@gmail.com>
2023-05-05 15:57:56 +0200

    Moved files to correct folders

---
+++ AppClasses/Math/FWEMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AnalysisITC
+{
+    public static class FWEMath
+    {
+        public static FloatWithError Log(FloatWithError number)
+        {
+            return new FloatWithError(Math.Log(number.Value), number.SD / number.Value);
+        }
+
+        public static FloatWithError Exp(FloatWithError number)
+        {
+            var f = Math.Abs(Math.Exp(number.Value));
+            var sd = Math.Abs(number.SD);
+
+            return new FloatWithError(f, f * sd);
+        }
+    }
+
+}
--- Utils/EnumExtensions.cs
+++ Utils/EnumExtensions.cs
@@ -147,22 +147,6 @@ namespace AnalysisITC
         }
     }
 
-    public static class FWEMath
-    {
-        public static FloatWithError Log(FloatWithError number)
-        {
-            return new FloatWithError(Math.Log(number.Value), number.SD / number.Value);
-        }
-
-        public static FloatWithError Exp(FloatWithError number)
-        {
-            var f = Math.Abs(Math.Exp(number.Value));
-            var sd = Math.Abs(number.SD);
-
-            return new FloatWithError(f, f * sd);
-        }
-    }
-
     public struct CGEdgeMargin
     {
         public nfloat Left { get; private set; }
