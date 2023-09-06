b26ab2fc85b7f40915d210cadc8f7c61163c0c46
frederiktheisen <FrederikTheisen@gmail.com>
2023-09-06 21:57:56 +0200

    Added kinetic model classes (not implemented)

---
+++ AppClasses/Analysis2/Kinetics/KineticModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AnalysisITC.AppClasses.Analysis2.Models;
+
+namespace AnalysisITC.AppClasses.Analysis2.Kinetics
+{
+	public class KineticModel
+	{
+		public Model Model { get; private set; }
+
+		public KineticModel(Model model)
+		{
+			Model = model;
+		}
+	}
+}
+
