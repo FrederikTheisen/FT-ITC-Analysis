268f28b35bf3462ee7ce0ea6f8754dc8dd89af55
frederiktheisen <FrederikTheisen@gmail.com>
2026-01-26 18:25:21 +0100

    Added ITCsim string resource

--- GUI/AppStrings.cs
+++ GUI/AppStrings.cs
@@ -17,6 +17,8 @@ namespace AnalysisITC.Utils
         public const string GibbsFreeEnergy = "∆*G*";
         public const string EntropyContribution = "-*T*∆*S*";
         public const string ProtonationEnthalpy = "∆*H*{buffer}";
+
+        public const string ITCsimName = "ITCsim";
     }
 
     /// <summary>
