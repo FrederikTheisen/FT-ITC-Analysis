07a48523b3d524348059bb4b6b156ecfee9c41c8
frederiktheisen <FrederikTheisen@gmail.com>
2026-01-26 13:29:52 +0100

    Added file

---
+++ AppClasses/AnalysisClasses/ParameterChecker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace AnalysisITC.AppClasses.AnalysisClasses
+{
+    public class ParameterChecker
+    {
+        public static bool IsWithinOnePercent(double parameter, double lowerLimit, double upperLimit)
+        {
+            // Handling the case where limits can be both positive and negative.
+            if (lowerLimit < 0 && upperLimit > 0)
+            {
+                return IsWithinRange(parameter, 0.99 * lowerLimit, 0.99 * upperLimit);
+            }
+
+            // Handling strictly positive parameters.
+            if (lowerLimit >= 0)
+            {
+                return IsWithinRange(parameter, 1.01 * lowerLimit, 0.99 * upperLimit);
+            }
+
+            // Handling strictly negative parameters.
+            if (upperLimit <= 0)
+            {
+                return IsWithinRange(parameter, 0.99 * lowerLimit, 1.01 * upperLimit);
+            }
+
+            return false;
+        }
+
+        private static bool IsWithinRange(double parameter, double lowerLimit, double upperLimit)
+        {
+            return parameter >= lowerLimit && parameter <= upperLimit;
+        }
+    }
+}
+
