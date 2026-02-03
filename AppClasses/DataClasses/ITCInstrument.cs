aa592d3b981e5270005af70ad0fe5bcae1d350e5
frederiktheisen <FrederikTheisen@gmail.com>
2026-02-03 12:02:45 +0100

    Added dead volume to instruments

--- AppClasses/DataClasses/ITCInstrument.cs
+++ AppClasses/DataClasses/ITCInstrument.cs
@@ -14,14 +14,22 @@ namespace DataReaders
         public string Extension { get; set; }
         public double StandardCellVolume { get; private set; }
         public double StandardSyringeVolume { get; private set; }
+        public double DeadVolume { get; private set; }
 
-        public ITCInstrumentAttribute(string name, string description, string extension, double cellv, double syrv)
+        public ITCInstrumentAttribute(string name, string description, string extension, double cellv, double syrv, double deadv = 0.0)
         {
             Name = name;
             Description = description;
             Extension = extension;
             StandardCellVolume = cellv / 1000000;
             StandardSyringeVolume = (syrv) / 1000000;
+
+            if (deadv < 1)
+            {
+                deadv = 0.4 * StandardCellVolume;
+            }
+
+            DeadVolume = (double)deadv;
         }
 
         public static List<ITCInstrument> GetITCInstruments()
@@ -31,6 +39,8 @@ namespace DataReaders
                 ITCInstrument.MicroCalITC200,
                 ITCInstrument.MalvernITC200,
                 ITCInstrument.MicroCalVPITC,
+                ITCInstrument.TAInstrumentsITCLowVolume,
+                ITCInstrument.TAInstrumentsITCStandard,
             };
         }
 
@@ -47,17 +57,40 @@ namespace DataReaders
 
     public enum ITCInstrument
     {
-        [ITCInstrument("Unknown", "", "", 200, 40)]
+        [ITCInstrument("Unknown", "", "",
+            cellv: 200,
+            syrv: 40,
+            deadv: 0)]
         Unknown,
-        [ITCInstrument("MicroCal ITC200", "", "ITC200_", 204, 39.5)]
+
+        [ITCInstrument("MicroCal ITC200", "", "ITC200_",
+            cellv: 204,
+            syrv: 39.5,
+            deadv: 80)]
         MicroCalITC200,
-        [ITCInstrument("MicroCal PEAQ-ITC", "", "MICROCALITC_MAL", 207.1, 39.0)]
+
+        [ITCInstrument("MicroCal PEAQ-ITC", "", "MICROCALITC_MAL",
+            cellv: 207.1,
+            syrv: 39.0,
+            deadv: 80)]
         MalvernITC200,
-        [ITCInstrument("MicroCal VP-ITC", "", "VPITC", 1479.1, 310)]
+
+        [ITCInstrument("MicroCal VP-ITC", "", "VPITC",
+            cellv: 1479.1,
+            syrv: 310,
+            deadv: 400)]
         MicroCalVPITC,
-        [ITCInstrument("TA Instruments ITC Standard", "", "TAITC", 1000.0, 250)]
+
+        [ITCInstrument("TA Instruments ITC Standard", "", "TAITC",
+            cellv: 1000.0,
+            syrv: 250,
+            deadv: 200)]
         TAInstrumentsITCStandard,
-        [ITCInstrument("TA Instruments ITC Low Vol", "", "TAITC", 190.0, 250)]
+
+        [ITCInstrument("TA Instruments ITC Low Vol", "", "TAITC",
+            cellv: 190.0,
+            syrv: 250,
+            deadv: 60)]
         TAInstrumentsITCLowVolume,
     }
 }
