3178161b87a466ab7aea2a83f3d74c9afe2a8336
frederiktheisen <FrederikTheisen@gmail.com>
2026-02-26 12:42:22 +0100

    Implemented more info in status

--- AppClasses/StateManager.cs
+++ AppClasses/StateManager.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using AnalysisITC.AppClasses.Analysis2;
 
 namespace AnalysisITC
@@ -10,6 +12,7 @@ namespace AnalysisITC
         public static event EventHandler UpdateStateDependentUI;
 
         private static ProgramState currentState = ProgramState.Load;
+        private static ProgramSubState currentSubState = ProgramSubState.None;
         public static ProgramState saveState { get; private set; } = ProgramState.Load;
         public static ProgramState CurrentState
         {
@@ -21,7 +24,25 @@ namespace AnalysisITC
                 UpdateStateDependentUI?.Invoke(null, null);
             }
         }
-        public static string ProgramStateString => Utilities.MarkdownStrings.AppName + " 〉" + CurrentState.ToString();
+        public static string ProgramStateString
+        {
+            get
+            {
+                string status = Utilities.MarkdownStrings.AppName + " 〉" + CurrentState.GetEnumDescription().ToString();
+
+                if (currentState == ProgramState.Analyze)
+                {
+                    if (ModelFactory.Factory.IsGlobalAnalysis) status += " 〉" + ProgramSubState.MultiAnalysis.GetEnumDescription() + $" [{DataManager.IncludedData.Count()} selected]";
+                }
+                else if (currentState == ProgramState.AnalysisView && currentSubState != ProgramSubState.None)
+                {
+                    status += " 〉" + currentSubState.GetEnumDescription().ToString();
+                }
+
+                return status;
+            }          
+        }
+        public static string ProgramSubStateString(ProgramSubState substate) => Utilities.MarkdownStrings.AppName + " 〉" + substate.GetEnumDescription().ToString();
 
         public static void Init()
         {
@@ -114,6 +135,13 @@ namespace AnalysisITC
             StatusBarManager.Invalidate();
         }
 
+        public static void SetProgramSubState(ProgramSubState subState)
+        {
+            currentSubState = subState;
+
+            StatusBarManager.Invalidate();
+        }
+
         public static void GoToResultView()
         {
             if (CurrentState != ProgramState.AnalysisView) saveState = CurrentState;
@@ -144,21 +172,69 @@ namespace AnalysisITC
         {
             DataManager.UndoDeleteData();
         }
+
+        public static void PromptHintPopup(Hint hint)
+        {
+            // Check if AppSettings shown hints contains hint string or int
+
+            // Determine if hint conditions are met
+
+            // Prompt hint
+
+            // Save hint as shown
+        }
     }
 
     public enum ProgramState
     {
+        [Description("Overview")]
         Load = 0,
+        [Description("Process")]
         Process = 1,
+        [Description("Analyze")]
         Analyze = 2,
+        [Description("Publish")]
         Publish = 3,
+        [Description("Analysis View")]
         AnalysisView = 4,
+        [Description("Always Active???")]
         AlwaysActive = 5,
+    }
+
+    public enum ProgramSubState
+    {
+        [Description("None")]
+        None,
+        [Description("Single")]
+        SingleAnalysis,
+        [Description("Multi")]
+        MultiAnalysis,
+        [Description("Structuring")]
+        ResultStructuring,
+        [Description("Salt Dependence")]
+        ResultSalt,
+        [Description("Protonation")]
+        ResultProtonation,
+        [Description("Tandem Merge Tool")]
+        MergeTool = 6,
+        [Description("Buffer Subtraction Tool")]
+        SubtractionTool = 7,
     }
 
     public enum UndoTask
     {
         DataManager,
         Analysis
+    }
+
+    public enum Hint
+    {
+        ExperimentDetails,              // Data was loaded, maybe a random chance
+        SpaceToCopyIntegrationLength,   // Peak selected, integration range changed
+        MultiDataFit,                   // Single fit performed with more than one dataset loaded
+        AnalysisDetails,                // Analysis result exists and random chance
+        Tools,                          // More than one epxeriment loaded and random chance
+        UseUnifiedAxes,                 // Final figure state and random chance
+        ExportDataToExternalPrograms,   // Save file completed
     }
 }
