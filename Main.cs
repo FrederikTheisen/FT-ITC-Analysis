eca7878447205390e28846052c4c714d1492614c
frederiktheisen <FrederikTheisen@gmail.com>
2026-03-09 17:23:29 +0100

    removed prints

--- Main.cs
+++ Main.cs
@@ -18,9 +18,7 @@ namespace AnalysisITC
             AppSettings.Locale = NSLocale.CurrentLocale.CollatorIdentifier; // We need to know for dates 
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US"); // We keep this for other formating...
 
-            Console.WriteLine("test1");
             CitationManager.TryFetchOnlineCitation();
-            Console.WriteLine("test2");
 
             // Not implemented yet if ever
             // BufferRegistry.Registry = BufferRegistry.LoadFromFile("./Buffers.json");
