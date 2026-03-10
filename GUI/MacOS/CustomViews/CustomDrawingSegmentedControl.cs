2b52f3d6c1d8e0fcd666946be95c6a94ad6649fc
frederiktheisen <FrederikTheisen@gmail.com>
2026-03-10 22:47:13 +0100

    Fixed color

--- GUI/MacOS/CustomViews/CustomDrawingSegmentedControl.cs
+++ GUI/MacOS/CustomViews/CustomDrawingSegmentedControl.cs
@@ -55,23 +55,29 @@ namespace AnalysisITC.GUI.MacOS.CustomViews
         {
             base.DrawSegment(segment, frame, controlView);
 
-            NSAttributedString str = null;
-            switch (segment)
+            var str = (long)segment switch
             {
-                default:
-                case 0: str = Utilities.MacStrings.AssociationConstant(Font); break;
-                case 1: str = Utilities.MacStrings.DissociationConstant(Font); break;
-            }
+                1 => Utilities.MacStrings.DissociationConstant(Font).MutableCopy() as NSMutableAttributedString,
+                _ => Utilities.MacStrings.AssociationConstant(Font).MutableCopy() as NSMutableAttributedString,
+            };
+
+            var isSelected = SelectedSegment == segment;
+
+            NSColor color;
+            if (isSelected)
+                color = NSColor.SelectedMenuItemText; // often closer to selected segmented text
+            else
+                color = NSColor.Label;
+
+            str.RemoveAttribute(NSStringAttributeKey.ForegroundColor, new NSRange(0, str.Length));
+            str.AddAttribute(NSStringAttributeKey.ForegroundColor, color, new NSRange(0, str.Length));
 
             var rect = str.BoundingRectWithSize(frame.Size, NSStringDrawingOptions.UsesLineFragmentOrigin);
 
-            // Calculate the center point for the string
-            var x = frame.Size.Width / 2 - rect.Size.Width / 2 + rect.Location.X;
-            var y = frame.Size.Height / 2 - rect.Size.Height / 2 + rect.Location.Y;
-            var point = new CGPoint(frame.X + x, y + 2);
+            var x = frame.X + (frame.Width - rect.Width) / 2.0f;
+            var y = frame.Y + (frame.Height - rect.Height) / 2.0f + 1.0f;
 
-            // Draw the attributed string at the center of the rectangle
-            str.DrawString(point);
+            str.DrawString(new CGPoint(x, y));
         }
     }
 }
