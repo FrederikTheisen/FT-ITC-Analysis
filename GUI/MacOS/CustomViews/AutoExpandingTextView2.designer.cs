7963b11baeda8674aafc44a1478a08de63f276d3
frederiktheisen <FrederikTheisen@gmail.com>
2026-07-02 23:54:59 +0200

    File restructuring

--- GUI/MacOS/CustomViews/AutoExpandingTextView2.designer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AnalysisITC.GUI.MacOS.CustomViews {
-
-	// Should subclass AppKit.NSView
-	[Foundation.Register("AutoExpandingTextView2")]
-	public partial class AutoExpandingTextView2 {
-	}
-}
--- GUI/MacOS/CustomViews/ExperimentAttributeView.designer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AnalysisITC.GUI.MacOS.CustomViews {
-
-	// Should subclass AppKit.NSView
-	[Foundation.Register("ExperimentAttributeView")]
-	public partial class ExperimentAttributeView {
-	}
-}
