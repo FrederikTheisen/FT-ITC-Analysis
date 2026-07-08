0c52943f932b2c57c0e40dcc88825e1a5fca0b93
frederiktheisen <FrederikTheisen@gmail.com>
2026-07-08 15:22:11 +0200

    Increased icon size

--- AnalysisITC.Avalonia/Misc/Dialogs/AboutDialogWindow.cs
+++ AnalysisITC.Avalonia/Misc/Dialogs/AboutDialogWindow.cs
@@ -26,8 +26,8 @@ internal sealed class AboutDialogWindow : Window
         var icon = new Image
         {
             Source = new Bitmap(AssetLoader.Open(new System.Uri("avares://AnalysisITC.Avalonia/Resources/appicon.ico"))),
-            Width = 100,
-            Height = 100,
+            Width = 120,
+            Height = 120,
             Margin = new Thickness(0, 0, 0, 12),
             HorizontalAlignment = HorizontalAlignment.Center
         };
